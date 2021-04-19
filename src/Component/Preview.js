4e9ac65ec98a5b44858791578f1ec7d48a0f2684
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-04-19 22:26:41 +0200

    added icons to preview component

--- src/Component/Preview.js
+++ src/Component/Preview.js
@@ -1,12 +1,56 @@
-import React from 'react'
-import './Preview.css'
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCameraImage, resetCameraImage } from "../features/cameraSlice";
+import "./Preview.css";
+import CloseIcon from "@material-ui/icons/Close";
+import TextFieldIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote"
+import AttachFileIcon from "@material-ui/icons/AttachFile"
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer"
+import SendIcon from "@material-ui/icons/Send"
 
 function Preview() {
-    return (
-        <div className='preview'>
-            
-        </div>
-    )
+  const cameraImage = useSelector(selectCameraImage);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!cameraImage) {
+      history.replace("/");
+    }
+  }, [cameraImage, history]);
+
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+  };
+
+  const sendPost = () => {
+    
+  }
+
+  return (
+    <div className="preview">
+      <CloseIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+          <TextFieldIcon />
+          <CreateIcon />
+          <NoteIcon />
+          <MusicNoteIcon />
+          <AttachFileIcon />
+          <CropIcon />
+          <TimerIcon />
+      </div>
+      <img src={cameraImage} alt="" />
+      <div onClick={sendPost} className="preview__footer">
+        <h2>Send Now</h2>
+        <SendIcon fontSize="small" className="preview__sendIcon" />
+      </div>
+    </div>
+  );
 }
 
-export default Preview
+export default Preview;
